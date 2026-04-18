446646738dccf8fbe3445b7a74175121c3724d1d
EagleWIFIm2 <alwaysrusya@gmail.com>
2026-04-18 20:20:16 +0300

    splitted apk releases

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -81,6 +81,15 @@ android {
         versionName = flutter.versionName
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86_64")
+            isUniversalApk = false
+        }
+    }
+
     buildTypes {
         getByName("debug") {
             applicationIdSuffix = ".debug"
